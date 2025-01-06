36539a8c5871519a8d66cf61f4fcac0f1e82a790
mehulambastha <mehul.213amb@gmail.com>
2025-01-06 23:42:03 +0530

    prod ready

--- app/sse/verify.py
+++ app/sse/verify.py
@@ -2,6 +2,7 @@ from fastapi import Security, HTTPException
 from fastapi.security.api_key import APIKeyHeader
 import os
 from dotenv import load_dotenv
+from ..logging import logger
 
 load_dotenv()
 
@@ -11,5 +12,6 @@ api_key_header = APIKeyHeader(name="X-API-Key")
 
 async def verify_api_key(api_key: str = Security(api_key_header)):
     if api_key != API_KEY:
+        logger.error(f"Wrong API Key in stream.. Provided {api_key}")
         raise HTTPException(status_code=403)
     return api_key
